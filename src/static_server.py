b53997ce8eaadae2d7efd1df24d40693f13ddf77
NumenZ <zerowow12@gmail.com>
2018-02-27 00:58:04 +0300

    Cleaning.

--- src/static_server.py
+++ src/static_server.py
@@ -3,10 +3,9 @@ from flask import (Flask, send_from_directory, make_response, render_template)
 app = Flask(__name__, template_folder='',static_url_path='', static_folder='')
 
 @app.route('/')
-def index_alias():
-    response = make_response(render_template('index.html', foo=42))
+def index():
+    response = make_response(render_template('static/html/index.html'))
     return response
-    # return send_from_directory('assets/web', 'index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
