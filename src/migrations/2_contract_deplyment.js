49af28179927d9691fb3d57a485e145998c2edfa
NumenZ <zerowow12@gmail.com>
2018-03-02 05:44:44 +0300

    L:AJSDL:KAS

--- src/migrations/2_contract_deplyment.js
+++ src/migrations/2_contract_deplyment.js
@@ -1,5 +1,5 @@
-let TwoPlayerGame = artifacts.require("./TwoPlayerGame.sol");
+let StickGame = artifacts.require("./StickGame.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(TwoPlayerGame);
+    deployer.deploy(StickGame);
 };
